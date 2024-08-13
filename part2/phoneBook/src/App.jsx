60e5509de6b4b66ea425515e7e1ad1cb79efa3aa
Gali Aljabari <galito43>
2024-08-13 14:55:04 +0300

    Add completed phonebook exercise 2.6-2.10

--- part2/phoneBook/src/App.jsx
+++ part2/phoneBook/src/App.jsx
@@ -1,13 +1,12 @@
 import { useEffect, useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", phoneNumber: "040 12345678" },
-  ]);
+  const [persons, setPersons] = useState([{ name: "Arto Hellas", phoneNumber: "040 12345678" }]);
   const [newName, setNewName] = useState("");
   const [existingPerson, setExisitngPerson] = useState(false);
   const [newPhoneNum, setNewPhoneNum] = useState("");
-  const []
+  const [searchField, setSearchField] = useState("");
+
   useEffect(() => {
     const personsArray = persons.map((person) => person.name);
     const personExists = personsArray.includes(newName);
@@ -22,55 +21,108 @@ const App = () => {
       : (() => {
           const person = {
             name: newName,
-            phoneNumber: newPhoneNum,
+            phoneNumber: newPhoneNum
           };
           setPersons(persons.concat(person));
           setNewName("");
           setNewPhoneNum("");
         })();
   };
-  const handleChange = (event) => {
+  const handleChange = (event, setState) => {
     console.log(event.target.value);
-    setNewName(event.target.value);
+    setState(event.target.value);
   };
 
-  const handlePhoneChange = (event) => {
-    console.log(event.target.value);
-    setNewPhoneNum(event.target.value);
-  };
-  const handleSearchCHange = (event) => {
-    
-  }
+  // const handlePhoneChange = (event) => {
+  //   console.log(event.target.value);
+  //   setNewPhoneNum(event.target.value);
+  // };
   return (
     <div>
       <h2>Phonebook</h2>
+      <Filter
+        searchField={searchField}
+        setSearchField={setSearchField}
+        handleChange={handleChange}
+      />
+      <PersonForm
+        actionOnSubmit={actionOnSubmit}
+        newName={newName}
+        setNewName={setNewName}
+        newPhoneNum={newPhoneNum}
+        setNewPhoneNum={setNewPhoneNum}
+        handleChange={handleChange}
+      />
+      <h2>Numbers</h2>
+      <DisplayPersons persons={persons} searchField={searchField} />
+    </div>
+  );
+};
+const DisplayPersons = ({ persons, searchField }) => {
+  return (
+    <div>
+      {persons
+        .filter((person) => {
+          return person.name.toLowerCase().includes(searchField.toLowerCase());
+        })
+        .map((filteredPerson) => {
+          return (
+            <div>
+              {filteredPerson.name} {filteredPerson.phoneNumber}
+            </div>
+          );
+        })}
+    </div>
+  );
+};
+const Filter = ({ searchField, setSearchField, handleChange }) => {
+  return (
+    <div>
       <label>filter shown wtih</label>
-      <input />
+      <input
+        value={searchField}
+        onChange={(event) => {
+          handleChange(event, setSearchField);
+        }}
+      />
+    </div>
+  );
+};
+export default App;
+
+const PersonForm = ({
+  actionOnSubmit,
+  newName,
+  setNewName,
+  newPhoneNum,
+  setNewPhoneNum,
+  handleChange
+}) => {
+  return (
+    <div>
       <form onSubmit={actionOnSubmit}>
         <div>
-          name: <input value={newName} onChange={handleChange} />
+          name:{" "}
+          <input
+            value={newName}
+            onChange={(event) => {
+              handleChange(event, setNewName);
+            }}
+          />
         </div>
         <div>
-          number: <input value={newPhoneNum} onChange={handlePhoneChange} />
+          number:{" "}
+          <input
+            value={newPhoneNum}
+            onChange={(event) => {
+              handleChange(event, setNewPhoneNum);
+            }}
+          />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
-      <DisplayPersons persons={persons} />
     </div>
   );
 };
-const DisplayPersons = ({ persons }) => {
-  return (
-    <div>
-      {persons.map((person) => (
-        <p key={Math.random()}>
-          {person.name} {person.phoneNumber}
-        </p>
-      ))}
-    </div>
-  );
-};
-export default App;
